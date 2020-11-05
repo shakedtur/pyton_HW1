e3a783f8a3822989b5a130b0daa2f824d64cbf2a
Lior Daichman <liordaichman@gmail.com>
2020-11-05 18:25:33 +0200

    add ex4,ex5,ex7

--- 313276859.py
+++ 313276859.py
@@ -1,7 +1,6 @@
-
 #ex1
+"""function that calculate the Xnor logic gate"""
 def Xnor(bool1,bool2):
-    """function that calculate the Xnor logic gate"""
     if(bool1==True and bool2==True):
         return True
     elif(bool1==False and bool2==False):
@@ -10,6 +9,14 @@ def Xnor(bool1,bool2):
         return False
 
 #ex2
+    """functin to calculate number according follwing roles
+        the function gets a number until 5 digits
+        number digits    action
+        1               same number
+        2               sum of two digits
+        3               sum of the first and last digits
+        4               sum of the two middle digits
+        5               show the middle digit"""
 def CountDigit(intnum):
     count=0
     while intnum >0:
@@ -26,14 +33,6 @@ def digitnum(number,locationum):
 
 
 def Digit(number5):
-    """functin to calculate number according follwing roles
-        the function gets a number until 5 digits
-        number digits    action
-        1               same number
-        2               sum of two digits
-        3               sum of the first and last digits
-        4               sum of the two middle digits
-        5               show the middle digit"""
     num=int(number5)
     counter = CountDigit(number5)
     print("the number of digits:", counter)
@@ -66,9 +65,6 @@ def IsEven(number1):
     else:
         return False
 
-
-
-
 def GoodOrder(intnumber):
     """boolean function gets a integer number
     return True when all the digis are even or odd"
@@ -88,35 +84,49 @@ def GoodOrder(intnumber):
 # assert GoodOrder(1573)is True
 print("good order?",GoodOrder(1573))
 
-
-#4 need to fix!!
+#ex4
 """print a piramid of number from one to the choose number """
 
-def UpAndDown(start):
-    if(start==1):
-        print(start,end='')
-    else:
-        print(start,end='')
-        UpAndDown(start-1)
-        print(start, end='')
+def figure(row_number):
+    numbers_from_start=1
+    numbers_from_end=row_number-1
+    count_from_second=2
+    for row in range (1,1+row_number):
+        for col in range (1,2*row_number):
+            if row+col==row_number+1 or col-row==row_number-1:
+                print("",numbers_from_start,end="")
+            elif row==row_number and col<=row_number:
+                print("", numbers_from_end, end="")
+                numbers_from_end = numbers_from_end - 1
+            elif row == row_number and col > row_number:
+                print("", count_from_second, end="")
+                count_from_second = count_from_second + 1
+            else:
+                print(end="  ")
+        numbers_from_start=numbers_from_start+1
+        print()
 
-print(UpAndDown(7))
+#ex5
+"""recursive function finding the highest number and the number of digits"""
 
-def PrintSpace(times):
-    print('_'*times,end='')
+def count_digit(number):
+    if number == 0:
+        return 0
+    return 1 + count_digit(number // 10)
 
-def Figure(edge):
-    #if(edge>0 and type(edge)==int):
-    for i in range(1,edge):
-        PrintSpace(edge-i)
-        for j in range(1,2):
-            print(i,end='')
-            for n in range(0,edge,2):
-                print()
+def higher_digit(number,max):
+    if number>0:
+        if number%10 > max:
+            max=number%10
+        return higher_digit(number//10,max)
+    return max
 
-#Figure(4)
+def weight(number):
+    c_d=count_digit(number)
+    h_d=higher_digit(number,0)
+    return c_d+h_d
 
-#6
+#ex 6
 def IsPrimary(primenum,check=2):
     """cheking prime number
     prime number= True
@@ -132,17 +142,24 @@ def IsPrimary(primenum,check=2):
 
 print("is prime?",IsPrimary(103))
 
+#ex7
+"""recursive function reduce the number 0"""
 
+def remove_digit( num, removedigit1):
+    if num==0:
+        return 0
+    if num%10!=removedigit1:
+        return num%10+remove_digit(num//10,removedigit1)*10
+    return remove_digit(num//10,removedigit1)
+#b=remove_digit(306098,0)
+#print("",b)
 
-
-
-#8
+#ex8
 
 '''
 A function that receives the coordinates of Pascal's triangle (x,y)
 and returns the value in the same coordinates.
 '''
-
 
 def Pascal(x, y):
     if x == y or y == 0:
